45589d7f46cd64cc85b3e225376d2ce8da198c09
erictempleton1 <erictempleton1@gmail.com>
2014-01-02 20:03:48 -0500

    fixed date issue with comments added

--- convert_date.py
+++ convert_date.py
@@ -1,21 +1,28 @@
-import datetime 
+import datetime
+from datetime import timedelta
 import time
-
 
 def date_monday():
     """ returns day of the week from 0-6, then subtracted from
-        current date to return monday's date for given week for any given week """
+        current date to return monday's date for given week for any given week.
+        switched to timedelta for when a new month starts midweek and causes problems
+        with prior months 31,30,29 ect. also added failsafe for first week
+        of a new year, which caused issues with 2013 to 2014 in same week."""
+
     now = datetime.datetime.now()
     day_number = datetime.datetime.today().weekday()
-    mon_date = int(now.day) - day_number
+    current_year = now.year
 
-    """ since the above subtracts from day_number 0-6, any date
-        less than or equal to 6 could break things """
-    if now.day <= 6:
-        day_diff = (int(now.day) + day_number) - day_number
-        return '%d/%s/%d' % (now.month, day_diff, now.year)
+    # failsafe for mondays with dates in prev months (31,30,29 ect)
+    mon_date = now - datetime.timedelta(days=day_number)
+    mon_num = mon_date.strftime('%d')
+    mon_year = int(mon_date.strftime('%Y'))
+    mon_monday = int(mon_date.strftime('%m'))
+
+    if current_year > mon_year:
+        return '%d/%s/%s' % (mon_monday, mon_num, mon_year)
     else:
-        return '%d/%s/%d' % (now.month, mon_date, now.year)
+        return '%d/%s/%s' % (now.month, mon_num, now.year)
 
 def epoch_date(n):
     """ converts any given date in m/d/y format to unix epoch.
