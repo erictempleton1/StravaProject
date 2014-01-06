f6272a81307c81e7bbf2505fd488b9f05220f265
erictempleton1 <erictempleton1@gmail.com>
2014-01-05 20:32:19 -0500

    major changes. moved all functions into a class. still need to do something with display func.

--- strava.py
+++ strava.py
@@ -3,7 +3,6 @@ import json
 import convert_date
 import datetime
 
-
 date_param = convert_date.epoch_date(convert_date.date_monday())
    
 payload = {'access_token': 'b295cfca0cfdf8f23d3a94707337f56b77ce7354', 'after': date_param}            
@@ -16,54 +15,59 @@ data = json.dumps(results, indent=4, sort_keys=True)
 # uncomment to write json response to file if needed     
 #with open('strava.txt', 'w')as f:  
    #f.write(data)  
+    
 
-# data in lists from strava api
-athlete_id = [ath_id['athlete']['id'] for ath_id in results]           
-moving_time = [time['moving_time'] / 60 for time in results]    
-distance = [items['distance'] * 0.000621371 for items in results]    
-date = [date['start_date'] for date in results]    
-map_polyline = [maps['map']['summary_polyline'] for maps in results]    
-calories = [cals['calories'] for cals in results]   
-activity_id = [act_id['id'] for act_id in results]
-avg_speed = [spd['average_speed'] for spd in results] 
-   
+class Calculations(object):
 
-def week_total_miles():   
-    return sum(distance)   
+    def __init__(self):
+        self.athlete_id = [ath_id['athlete']['id'] for ath_id in results]           
+        self.moving_time = [time['moving_time'] / 60 for time in results] # secs to mins   
+        self.distance = [items['distance'] * 0.000621371 for items in results] # meters to miles   
+        self.date = [date['start_date'] for date in results]    
+        self.map_polyline = [maps['map']['summary_polyline'] for maps in results]    
+        self.calories = [cals['calories'] for cals in results]   
+        self.activity_id = [act_id['id'] for act_id in results]
+        self.avg_speed = [spd['average_speed'] for spd in results] 
+
+    def time_list(self):
+        return self.moving_time
+
+    def week_total_miles(self):   
+        return sum(self.distance)   
         
-def avg_miles():   
-    return sum(distance) / len(distance)   
+    def avg_miles(self):   
+        return sum(self.distance) / len(self.distance)   
         
-def miles_remaining(n):  
-    """ change n (goal) as needed """   
-    return n - week_total_miles()   
+    def miles_remaining(self, n):  
+        """ change n (goal) as needed """   
+        return n - self.week_total_miles()   
         
-def week_total_time():   
-    return sum(moving_time)
+    def week_total_time(self):   
+        return sum(self.moving_time)
  
-def days_remaining():
-    """ added if's to deal with 0's produced by dividing by 0 on sundays """
-    day_today = datetime.datetime.today().weekday() + 1
-    if day_today == 7:
-        return 0
-    else:
-        return 7 - day_today
+    def days_remaining(self):
+        """ added if's to deal with 0's produced by dividing by 0 on sundays """
+        day_today = datetime.datetime.today().weekday() + 1
+        if day_today == 7:
+            return 0
+        else:
+            return 7 - day_today
  
-def avg_to_goal():
-    """ same as above, added if's to deal with 0's produced on sundays """
-    if days_remaining() == 0:
-        return 0
-    else:
-        return miles_remaining(50) / days_remaining()
+    def avg_to_goal(self):
+        """ same as above, added if's to deal with 0's produced on sundays """
+        if self.days_remaining() == 0:
+            return 0
+        else:
+            return self.miles_remaining(50) / self.days_remaining()
 
-def avg_pace():
-    return sum(moving_time) / sum(distance)
-     
+    def avg_pace(self):
+        return sum(self.moving_time) / sum(self.distance)
+
 def display_results():  
           
     date = datetime.datetime.now()  
       
-    print 'Weekly Totals as of %s:' % date.isoformat()
+    print 'Weekly Totals as of %s:' % date.strftime('%m/%d/%Y')
     print '%.1f miles' % week_total_miles()
     print '%.1f miles remaining to goal' % miles_remaining(50)
     print '%.1f mins total' % week_total_time()   
@@ -72,6 +76,10 @@ def display_results():
     print '%.0f days remaining this week after today' % days_remaining()
     print '%.1f miles per day to reach weekly goal' % avg_to_goal()
     print '%.2f mins/mile avg pace' % avg_pace()
- 
- 
-display_results()
+
+
+
+
+
+
+
