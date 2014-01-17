d2473ccfc59581e3957d1a56ebedc4d6b6b7e9c9
erictempleton1 <erictempleton1@gmail.com>
2014-01-17 15:44:06 -0500

    wsgi upated

--- flask_strava.py
+++ flask_strava.py
@@ -1,3 +1,13 @@
+import sys, os
+INTERP = os.path.join(os.environ['HOME'], 'django.runthescript.com', 'bin', 'python')
+if sys.executable != INTERP:
+    os.execl(INTERP, INTERP, *sys.argv)
+sys.path.append(os.getcwd())
+ 
+sys.path.append('django')
+from django import app as application
+
+
 import datetime
 from strava import Calcs
 from flask import Flask, render_template
