7cbae6bf0655ff6f8b1a6c92c425dec8e1243358
unknown <eric.templeton@DWUS01LT01770.totalent>
2014-01-22 16:49:58 -0500

    testing loops for layout

--- week_layout.py
+++ week_layout.py
@@ -3,9 +3,13 @@ from strava_auth import Auth
 
 auth = Auth().connect()
 
-date = [date['start_date'][:10] for date in auth]
+date = [date['start_date'][:10] for date in auth] # [:10] drops extra time info
 distance = [items['distance'] * 0.000621371 for items in auth]
 
 # converts given date to it day name in the week ie: monday, tuesday, friday ect
-for items in date:
-    print datetime.datetime.strptime(items, '%Y-%m-%d').strftime('%A')
+for dates in date:
+    print datetime.datetime.strptime(dates, '%Y-%m-%d').strftime('%A')
+
+for miles in distance:
+    print '%.02f' % miles
+    
