9a2ad81b7c5779cdbd974370f0086224703c781b
unknown <eric.templeton@DWUS01LT01770.totalent>
2014-01-22 17:26:11 -0500

    added zip for printing

--- week_layout.py
+++ week_layout.py
@@ -7,8 +7,5 @@ date = [date['start_date'][:10] for date in auth] # [:10] drops extra time info
 distance = [items['distance'] * 0.000621371 for items in auth]
 
 # converts given date to it day name in the week ie: monday, tuesday, friday ect
-for dates in date:
-    print datetime.datetime.strptime(dates, '%Y-%m-%d').strftime('%A')
-
-for miles in distance:
-    print '%.02f' % miles
+for dates, miles in zip(date, distance):
+    print datetime.datetime.strptime(dates, '%Y-%m-%d').strftime('%A'), '%.02f' % miles
