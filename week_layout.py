290d0296b94e2bb445d3cd59373eb05a4334abea
erictempleton1 <erictempleton1@gmail.com>
2014-01-22 20:10:13 -0500

    added day number and potential idea

--- week_layout.py
+++ week_layout.py
@@ -11,3 +11,8 @@ date_distance = zip(date, distance)
 # converts given date to it day name in the week ie: monday, tuesday, friday ect
 for dates, miles in date_distance:
     print datetime.datetime.strptime(dates, '%Y-%m-%d').strftime('%A'), '%.02f' % miles
+
+day_number = datetime.datetime.today().weekday()
+print day_number
+
+# figure out how to use this with range to slice in dates for the rest of the week
