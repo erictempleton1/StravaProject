71cfbe08b853056e4f9255d2b163cb22857f4fb1
erictempleton1 <erictempleton1@gmail.com>
2014-01-24 14:17:55 -0500

    still working onprinting issue

--- week_layout.py
+++ week_layout.py
@@ -6,28 +6,19 @@ auth = Auth().connect()
 date = [date['start_date'][:10] for date in auth] # [:10] drops extra time info
 distance = [items['distance'] * 0.000621371 for items in auth]
 
-date_distance = zip(date, distance)
 
 # converts given date to it day name in the week ie: monday, tuesday, friday ect
-for dates, miles in date_distance:
-    print datetime.datetime.strptime(dates, '%Y-%m-%d').strftime('%A'), '%.02f' % miles
+date_distance = zip(date, distance)
 
-for dates in date:
-    print datetime.datetime.strptime(dates, '%Y-%m-%d').weekday()
+date_names = [datetime.datetime.strptime(dates, '%Y-%m-%d').strftime('%A') for dates in date]
 
-day_dict = {0:'Monday', 1:'Tuesday', 2:'Wednesday',
-            3:'Thursday', 4:'Friday', 5:'Saturday',
-            6:'Sunday'}
+date_num = [datetime.datetime.strptime(dates, '%Y-%m-%d').weekday() for dates in date]
 
+day_list = ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
+            'Friday', 'Saturday', 'Sunday']
 
-"""
- # try to use something like this to compare dict and list
-
-for key in mylist:
-    if key in mydict1:
-        print 'Key %s in dict 1' % key
-    elif key in mydict2:
-        print 'Key %s in dict 2' % key
+for names in day_list:
+    if date_names in day_list:
+        print date_names[names], distance[names]
     else:
-        print 'Key %s not in dictionaries' % key
-"""
+    
