1bce29394654d2d23b00b0471b35b660cf29edca
erictempleton1 <erictempleton1@gmail.com>
2014-02-01 19:39:18 -0500

    testing issue

--- pace_test.py
+++ pace_test.py
@@ -1,65 +1,20 @@
-from strava_auth import Auth
+import requests
 import datetime
-
-auth = Auth().connect()
-
-distance = [items['distance'] * 0.000621371 for items in auth]
-moving_time = [time['moving_time'] for time in auth]
-date = [date['start_date'][:10] for date in auth]
-
-def last_date():
-    """ handles error when there is no runs for a week yet """
-
-    try:
-        last_run = int(datetime.datetime.strptime(date[-1], '%Y-%m-%d').strftime('%d'))
-
-    except IndexError:
-        last_run = 0
-
-    return last_run
+import convert_date
+import json
 
 
-def days_remaining():
-    """ added if's to deal with 0's produced by dividing by 0 on sundays """
+date_param = convert_date.epoch_date(convert_date.date_monday())
 
-    # returns day of the month from 1-31
-    day_num = int(datetime.datetime.now().strftime('%d'))
+payload = {'access_token': 'b295cfca0cfdf8f23d3a94707337f56b77ce7354',
+                   'after': date_param}
 
-    # return day of the week from 1-7
-    day_today = int(datetime.datetime.today().isoweekday())
+r = requests.get('https://www.strava.com/api/v3/athlete/activities', 
+                          params=payload)
 
-    # last_date() returns day of the month for last month
-    # accounts for if you have ran on a given day
-    if day_num == last_date():
-        return 7 - day_today
+results = r.json()
 
-    # accounts for extra day if no run for the day yet
-    else:
-        return 8 - day_today
-
-def week_goal():
-    return 55
-
-def week_total_miles():   
-    return sum(distance) 
-
-def miles_remaining():
-        """ change n (goal) as needed """
-        miles_remaining = week_goal() - week_total_miles()
-        return int(miles_remaining)
-
-
-# need to manually enter vars to test this func
-def avg_to_goal():
-    """ same as above, added if's to deal with 0's produced on sundays """
-    if days_remaining() == 0:
-        return 0
-    else:
-        return float(miles_remaining()) / float(days_remaining())
-
-print avg_to_goal()
-
-
+print results
 
 
 
