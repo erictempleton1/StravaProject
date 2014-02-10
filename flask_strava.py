b40c60e6d09b11c3b5da6ee3a0581576add406d3
erictempleton1 <erictempleton1@gmail.com>
2014-02-10 15:25:59 -0500

    finishing up contact form

--- flask_strava.py
+++ flask_strava.py
@@ -49,9 +49,15 @@ def contact():
     if request.method == 'POST':
         if form.validate() == False:
             flash('All fields are required')
-            return render_template('contact.html', form=form)    
-        return 'Form posted'
-
+            return render_template('contact.html', form=form)
+        else: 
+            msg = Message(form.subject.data, sender = 'milesdash4@gmail.com', 
+                        recipients = ['milesdash4@gmail.com'])
+            msg.body = """From: %s <%s> %s""" % (form.name.data, form.email.data,
+                                              form.message.data)
+            mail.send(msg)
+            return render_template('contact.html', success = True)
+        
     elif request.method == 'GET':
         return render_template('contact.html', form=form)
 
