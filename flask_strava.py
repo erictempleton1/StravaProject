ee2c1b635baec3807d1e0d45c6ce693ebf167759
erictempleton1 <erictempleton1@gmail.com>
2014-04-05 16:37:46 -0400

    moving goals form to main page

--- flask_strava.py
+++ flask_strava.py
@@ -1,53 +1,52 @@
 import datetime
 from strava import Calcs
 from flask import Flask, render_template, request, redirect, flash
-from forms import ContactForm, GoalForm
+from forms import GoalForm
 from flask_mail import Message, Mail
 
 app_strava = Flask(__name__)
 app_strava.secret_key = 'dev key'
 
-class Routes(object):
 
-    @app_strava.route('/', methods=['GET', 'POST'])
-    def index_strava():
-        calcs = Calcs()
-        form = GoalForm()
+@app_strava.route('/', methods=['GET', 'POST'])
+def index_strava():
+    calcs = Calcs()
+    form = GoalForm()
 
-        date = datetime.datetime.now()  
-      
-        week_date = 'Weekly Totals as of %s:' % date.strftime('%m/%d/%Y')
-        week_miles = '%.1f miles' % calcs.week_total_miles()
-        week_total_time = '%s total time' % calcs.time_list() 
-        num_runs = calcs.num_runs()
-        avg_miles = '%.1f miles/run' % calcs.avg_miles() 
-        avg_pace = '%s mins/mile avg pace' % calcs.avg_pace()
+    date = datetime.datetime.now()  
+  
+    week_date = 'Weekly Totals as of %s:' % date.strftime('%m/%d/%Y')
+    week_miles = '%.1f miles' % calcs.week_total_miles()
+    week_total_time = '%s total time' % calcs.time_list() 
+    num_runs = calcs.num_runs()
+    avg_miles = '%.1f miles/run' % calcs.avg_miles() 
+    avg_pace = '%s mins/mile avg pace' % calcs.avg_pace()
 
-        # Mon - Sun per day miles including doubles
-        week_layout = calcs.week_layout()
+    # Mon - Sun per day miles including doubles
+    week_layout = calcs.week_layout()
 
-        week_totals = [week_miles, num_runs, week_total_time, avg_miles, avg_pace]
+    week_totals = [week_miles, num_runs, week_total_time, avg_miles, avg_pace]
 
-        return render_template('strava.html', week_date=week_date, week_totals=week_totals,
-                                week_layout=week_layout, title='Home')
+    return render_template('strava.html', week_date=week_date, week_totals=week_totals,
+                            week_layout=week_layout, title='Home', form=form)
 
 
-    @app_strava.route('/goals', methods = ['GET', 'POST'])
-    def form_test():
-        calcs = Calcs()
-        form = GoalForm()
+@app_strava.route('/goals', methods = ['GET', 'POST'])
+def goals():
+    calcs = Calcs()
+    form = GoalForm()
 
-        if request.method == 'POST' and form.validate() == True:
-            goal = form.goal.data
-            avg_to_goal = '%.1f miles/day to reach goal' % calcs.avg_to_goal(goal)
-            miles_remain = '%.1f miles remain' % calcs.miles_remaining(goal)
-            days_remain = '%.0f days remaining' % calcs.days_remaining()
-            week_goal = '%.0f miles' % calcs.week_goal(goal)
-            week_goals = [week_goal, miles_remain, days_remain, avg_to_goal]
-            return render_template('goals.html', week_goals=week_goals, success=True, form=form, title='Goals')
-        else:
-            flash('Please enter a number from 1-100')
-            return render_template('goals.html', form=form, title='Goals')
+    if request.method == 'POST' and form.validate() == True:
+        goal = form.goal.data
+        avg_to_goal = '%.1f miles/day to reach goal' % calcs.avg_to_goal(goal)
+        miles_remain = '%.1f miles remain' % calcs.miles_remaining(goal)
+        days_remain = '%.0f days remaining' % calcs.days_remaining()
+        week_goal = '%.0f miles' % calcs.week_goal(goal)
+        week_goals = [week_goal, miles_remain, days_remain, avg_to_goal]
+        return render_template('strava.html', week_goals=week_goals, success=True, form=form, title='Goals')
+    else:
+        flash('Please enter a number from 1-100')
+        return render_template('strava.html', form=form, title='Goals')
 
 if __name__ == '__main__':
     app_strava.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,13 +1,5 @@
 from flask.ext.wtf import Form
 from wtforms import TextField, TextAreaField, SubmitField, IntegerField, validators
 
-class ContactForm(Form):
-    name = TextField('Name', [validators.Required('Please enter your name')])
-    email = TextField('Email', [validators.Required('Please enter your email'), validators.Email('Please enter your email')])
-    subject = TextField('Subject', [validators.Required('Please enter a subject')])
-    message = TextAreaField('Message', [validators.Required('Please enter a message')])
-    submit = SubmitField('Send')
-
 class GoalForm(Form):
     goal = IntegerField('Week Goal', [validators.NumberRange(min=1, max=150), validators.Required('Please enter a number from 1 to 150')])
-    submit = SubmitField('Submit')
